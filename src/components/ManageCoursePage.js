a51ccc987d8e6f874b41f55d340f3a46bdc99663
Caio César Silva <caiocesarsilva08@gmail.com>
2020-04-06 20:36:53 -0300

    Flux
    Implementation of Flux usabilities to make the unidirectional data flow work with my application.

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
-import * as courseApi from "../api/courseApi";
+import courseStore from "../stores/courseStore";
 import { toast } from "react-toastify";
+import * as courseActions from "../actions/courseActions";
 
 const ManageCoursePage = (props) => {
   const [errors, setErrors] = useState({});
+  const [courses, setCourses] = useState(courseStore.getCourses());
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -14,11 +16,23 @@ const ManageCoursePage = (props) => {
   });
 
   useEffect(() => {
+    //i want to call the onChange function when the courseStore changes
+    courseStore.addChangeListener(onChange);
     const slug = props.match.params.slug; //from the path '/courses/:slug'
-    if (slug) {
-      courseApi.getCourseBySlug(slug).then((_course) => setCourse(_course));
+    //if there aren't any courses in the state, we call loadCourses()
+    if (courses.length === 0) {
+      courseActions.loadCourses();
+    } else if (slug) {
+      //ask the store by it slug and populate the form
+      setCourse(courseStore.getCourseBySlug(slug));
     }
-  }, [props.match.params.slug]);
+    //we clean up here, by returning a function
+    return () => courseStore.removeChangeListener(onChange);
+  }, [courses.length, props.match.params.slug]);
+
+  function onChange() {
+    setCourses(courseStore.getCourses());
+  }
 
   /**here we are using destructuring in the parameters of the function, so:
    * const { target } = event.target
@@ -50,7 +64,7 @@ const ManageCoursePage = (props) => {
     //this will prevent the page from posting back to the server
     event.preventDefault();
     if (!formIsValid()) return;
-    courseApi.saveCourse(course).then(() => {
+    courseActions.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course saved.");
     });
---
+++ src/stores/courseStore.js
@@ -0,0 +1,67 @@
+import { EventEmitter } from "events";
+import Dispatcher from "../appDispatcher";
+import actionTypes from "../actions/actionTypes";
+
+const CHANGE_EVENT = "change";
+let _courses = [];
+
+class CourseStore extends EventEmitter {
+  /**this will allow React components to subscribe to our store
+   * so they're notified when changes occur */
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  /** this will allow React components to unsubscribe from our store*/
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+
+  getCourses() {
+    return _courses;
+  }
+
+  getCourseBySlug(slug) {
+    return _courses.find((course) => course.slug === slug);
+  }
+}
+
+const store = new CourseStore();
+
+Dispatcher.register((action) => {
+  switch (action.actionType) {
+    case actionTypes.CREATE_COURSE:
+      _courses.push(action.course);
+      store.emitChange();
+      break;
+    case actionTypes.UPDATE_COURSE:
+      /**map will iterate over the courses array and ask if the course of the array
+       * have the same id of action.course, if not, it does nothing, if it have,
+       * so the course of the array receives the data of the action.course
+       */
+      _courses = _courses.map((course) =>
+        course.id === action.course.id ? action.course : course
+      );
+      store.emitChange();
+      break;
+    case actionTypes.LOAD_COURSES:
+      _courses = action.courses;
+      store.emitChange();
+      break;
+    case actionTypes.DELETE_COURSE:
+      //iterate over all the courses and filter out any course that it's id match the action course id
+      _courses = _courses.filter(
+        (course) => course.id !== parseInt(action.id, 10)
+      );
+      store.emitChange();
+      break;
+    default:
+    //nothing to do here
+  }
+});
+
+export default store;
